849b6bd5a177324ecccf6738d990fb74e4435b66
Anurag Kumar <anurag.kumar@hypertest.co>
2023-08-28 12:13:31 +0530

    changes
    Signed-off-by: Anurag Kumar <anurag.kumar@hypertest.co>

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,15 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
-# Check if the current branch is master
-if [ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]; then
-    # Check if the current branch is in sync with master
-    if git diff master --exit-code; then
-        echo "Branch is in sync with master. Commit allowed."
-        exit 0
-    else
-        echo "Branch is not in sync with master. Commit not allowed."
-        exit 1
-    fi
+BASE_BRANCH="master"
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# Check if we're already on the baseline branch
+if [ "$CURRENT_BRANCH" = "$BASE_BRANCH" ]; then
+  exit 0
+fi
+
+# Fetch latest changes
+git fetch
+
+# Check if the current branch is behind the baseline branch
+BEHIND=$(git rev-list --left-right --count $BASE_BRANCH...$CURRENT_BRANCH | awk '{print $1}')
+
+if [ "$BEHIND" -ne 0 ]; then
+  echo "\nError: Your branch is not up-to-date with $BASE_BRANCH."
+  echo "Please take a pull of the concerned branch and try again.\n"
+  exit 1
 fi
 
 exit 0
